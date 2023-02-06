1381846dfef214a850f9cfffabc7dd75a4ee67cb
Cecil Mahumane <cecmahumane@gmail.com>
2023-02-06 08:11:03 -0500

    env staging changed

--- server/db.js
+++ server/db.js
@@ -3,9 +3,9 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   user: process.env.USER,
   password: process.env.PASSWORD,
-  host: process.env.HOST,
+  host: process.env.DATABASE_URL,
   port: process.env.DBPORT,
-  database: process.env.DATABASE_URL,
+  database: process.env.DATABASE,
 });
 
 module.exports = pool;
